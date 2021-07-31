31d83ba0697c0673952407354dab0e04b230397c
nany sama <natalia.samaniego.95@gmail.com>
2021-07-31 15:10:42 -0300

    conexion

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Api UP')
+    //res.send('Api UP')
     dbTickets = db.getInstance();
     dbTickets.collection("tickets")
         .find()
